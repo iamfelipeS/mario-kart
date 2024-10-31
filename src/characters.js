071b1fcdd86a620c76854ccae2b5a830ba123302
iamfelipeS <flipe.silva@terra.com.br>
2024-10-31 00:13:53 -0300

    add: character array

---
+++ src/characters.js
@@ -0,0 +1,52 @@
+const characters = [
+    {
+        code: 1,
+        name: "Mario",
+        speed: 4,
+        maneuverability: 3,
+        power: 3,
+        points: 0,
+    },
+    {
+        code: 2,
+        name: "Peach",
+        speed: 3,
+        maneuverability: 4,
+        power: 2,
+        points: 0,
+    },
+    {
+        code: 3,
+        name: "Yoshi",
+        speed: 2,
+        maneuverability: 4,
+        power: 3,
+        points: 0,
+    },
+    {
+        code: 4,
+        name: "Bowser",
+        speed: 5,
+        maneuverability: 2,
+        power: 5,
+        points: 0,
+    },
+    {
+        code: 5,
+        name: "Luigi",
+        speed: 3,
+        maneuverability: 4,
+        power: 4,
+        points: 0,
+    },
+    {
+        code: 6,
+        name: "Donkey Kong",
+        speed: 2,
+        maneuverability: 2,
+        power: 5,
+        points: 0,
+    },
+];
+
+export default characters;
